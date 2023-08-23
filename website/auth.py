02332a8d0f49e5861a27ab75d3cc8c7af2da5947
trojanX12388 <robertandrewb.up@gmail.com>
2023-08-23 16:37:16 +0800

    FIS Updated V1.1

--- website/auth.py
+++ website/auth.py
@@ -3,6 +3,21 @@ from flask import Blueprint, render_template
 auth = Blueprint('auth', __name__)
 
 
-@auth.route("/")
-def home():
-    return render_template("Welcome-Page/index.html", name="Andrew")
+@auth.route("/admin-login")
+def adminL():
+    return ("<h1>Admin Login</h1>")
+
+
+@auth.route("/faculty-login")
+def facultyL():
+    return ("<h1>Faculty Login</h1>")
+
+
+@auth.route("/logout")
+def Logout():
+    return ("<h1>Logged Out</h1>")
+
+
+@auth.route("/sign-up")
+def signUp():
+    return ("<h1>Sign Up</h1>")
