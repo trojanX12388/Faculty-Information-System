afdad7dcf9b9a46ef8b9f0b96fa586ef3a2202e5
trojanX12388 <robertandrewb.up@gmail.com>
2023-08-27 02:34:00 +0800

    JSON

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,16 @@
-from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask import Blueprint, flash, redirect, render_template, request, jsonify, url_for
 import psycopg2
+
+# DATABASE CONNECTION
+
 conn = psycopg2.connect(host="34.72.164.60", dbname="FIS", user="postgres",
                                 password="plazma12388", port=5432)
 
-auth = Blueprint('auth', __name__)
+# -------------------------------------------------------------
 
+# WEB AUTH ROUTES URL
+
+auth = Blueprint('auth', __name__)
 
 @auth.route("/admin-login", methods=['GET', 'POST'])
 def adminL():
@@ -38,6 +44,11 @@ def facultyH():
     return render_template("Faculty-Home-Page/home.html")
 
 
+@auth.route("/faculty-forgot-pass")
+def facultyF():
+    return ("<h1>Forgot Password</h1>")
+
+
 @auth.route("/logout")
 def Logout():
     return ("<h1>Logged Out</h1>")
@@ -46,3 +57,35 @@ def Logout():
 @auth.route("/sign-up")
 def signUp():
     return ("<h1>Sign Up</h1>")
+
+
+# -------------------------------------------------------------
+
+# JSON DATA API
+
+# JSON DATA SAMPLE FROM GET "user_id"
+
+@auth.route("/get-user/<user_id>")
+def get_user(user_id):
+    user_data = {
+        "user_id": user_id,
+        "name": "Alma Matter",
+        "email": "alma123@gmail.com"
+        }
+    
+    extra = request.args.get("extra")
+    if extra:
+        user_data["extra"] = extra
+    
+    return jsonify(user_data), 200
+
+
+# JSON POST METHOD
+
+@auth.route("/create-user", methods=["POST"])
+def create_user():
+    data = request.get_json()
+    
+    return jsonify(data), 201
+
+# -------------------------------------------------------------
