a634c579f9b0eebc4ccfe707d0dbe7bea0467b48
trojanX12388 <robertandrewb.up@gmail.com>
2023-09-03 19:14:38 +0800

    EXECUTING MODELS SAMPLE CONFIG

--- website/__init__.py
+++ website/__init__.py
@@ -7,6 +7,16 @@ load_dotenv()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    
+    # CONFIGURING POSTGRESQL CONNECTIONS
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    # LOADING DATABASE 
+    from website.models import init_db
+    init_db(app)
+    
+    # IMPORTING ROUTES
     from .views import views
     from .auth import auth
     
