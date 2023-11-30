b8d8a06a6fdf52588159f8858dff7c594fb05f64
Robert Andrew Bardoquillo <142313473+trojanX12388@users.noreply.github.com>
2023-11-30 15:29:42 +0800

    UPDATE WITH FAST API

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from website import create_app
+from website.API.fastapi import fastapi_app
 
 app = create_app()
 
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000)
-    
+    from threading import Thread
+    from uvicorn import run as uvicorn_run
+
+    # Start Flask in a separate thread
+    def start_flask():
+        app.run(host='0.0.0.0', port=8000)
+
+    flask_thread = Thread(target=start_flask)
+    flask_thread.start()
+
+    # Start FastAPI using Uvicorn
+    uvicorn_run(fastapi_app, host='127.0.0.1', port=7000)
