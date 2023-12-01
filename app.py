bed62bedd4bc81b9eccdc653429ab5a4cd8cc8f1
trojanX12388 <robertandrewb.up@gmail.com>
2023-12-01 11:23:24 +0800

    UPDATE APP.py

--- app.py
+++ app.py
@@ -3,11 +3,6 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     
-    app.run(host="0.0.0.0", port=8000, debug=True)
-    
-=======
     app.run(host="0.0.0.0", port=8000)
     
->>>>>>> 7517c7c95bad6e715d7b4f285ba737c2ed118027
