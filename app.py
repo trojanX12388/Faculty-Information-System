ceee1d65c26dd36459fefea82ce59f50ad689f6a
trojanX12388 <robertandrewb.up@gmail.com>
2023-12-01 11:18:40 +0800

    Committing changes before pull

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-<<<<<<< HEAD
+    
     app.run(host="0.0.0.0", port=8000, debug=True)
     
-=======
-    app.run(host="0.0.0.0", port=8000)
-    
->>>>>>> 29dc4f5836f57b1a06faf1acba69f280f7a38a00
